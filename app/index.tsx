d9307db5320e4666b3cb77e4c566b222ccbd5ca4
Ra1NuX <ra1nux.01@gmail.com>
2023-06-24 19:16:14 +0200

    first commit 🌑

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+import { SafeAreaView  } from 'react-native-safe-area-context';
+
+export default function IndexPage() {
+  return (
+    <SafeAreaView>
+        <View>
+            <Text className="" >Hola</Text>
+        </View>
+    </SafeAreaView>
+  )
+}
